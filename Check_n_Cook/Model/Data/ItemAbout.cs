0dfeead2001e7282650bfd1b0aef09b266615cac
ScaY <stephane.viper@gmail.com>
2014-06-06 14:41:51 +0200

    Added design

---
+++ Check_n_Cook/Model/Data/ItemAbout.cs
@@ -0,0 +1,25 @@
+﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_n_Cook.Model.Data
+{
+    public class ItemAbout
+    {
+        public string Image { get; set; }
+
+        public string Title { get; set; }
+
+        public string Subtitle { get; set; }
+
+        public ItemAbout(string image, string title, string subTitle)
+        {
+            this.Image = image;
+            this.Title = title;
+            this.Subtitle = subTitle;
+        }
+    }
+}
---
+++ Check_n_Cook/Model/Data/ItemResult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_n_Cook.Model.Data
+{
+    public class ItemResult
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private string image;
+
+        public string Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+
+    }
+}
