3bdc9782fab9025016ae69f2c795e9f86a3de668
Hugo DUFOSSEZ <hdu.web@gmail.com>
2014-06-11 11:20:37 +0200

    ok

---
+++ Check_n_Cook/Model/PlanningEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_n_Cook.Model
+{
+    class PlanningEntry
+    {
+        public DateTime PlanningDate { get; set; }
+
+        public PlanningType PlanningType { get; set; }
+
+        public PlanningEntry()
+        {
+
+        }
+
+        public PlanningEntry(DateTime date, PlanningType plantype)
+        {
+            this.PlanningDate = date;
+            this.PlanningType = plantype;
+        }
+    }
+}
