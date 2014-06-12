5238fca8f73cd222f5febf89b2aea1ad55c97f2d
Hugo DUFOSSEZ <hdu.web@gmail.com>
2014-06-12 11:31:10 +0200

    Revert "commit serialiser for receipe"
    This reverts commit a205b150b8ce6aa5178a1bb280be48352bfacf33.

--- Check_n_Cook/Model/Receipe.cs
+++ Check_n_Cook/Model/Receipe.cs
@@ -4,16 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Data.Json;
-using System.Runtime.Serialization.Json;
-using System.IO;
-using Windows.Storage;
-using Windows.Storage.Streams;
-using System.Runtime.Serialization;
 
 namespace Check_n_Cook.Model
 {
-    [DataContract]
     public class Receipe
     {
         public int Id { get; set; }
@@ -29,8 +22,6 @@ namespace Check_n_Cook.Model
         public Rating Rating { get; set; }
 
         public Difficulty Difficulty { get; set; }
-
-        public int fileId { get; set; }
 
         public Cost Cost { get; set; }
 
@@ -66,8 +57,6 @@ namespace Check_n_Cook.Model
             this.Vegetarian = vegetarian;
             this.WithAlcohol = withAlcohol;
             this.PlanningEntry = null;
-            AddToPlan(new PlanningEntry(DateTime.Today, PlanningType.MIDI));
-
         }
         public Receipe(String title, User author, DateTime publicationDate, DishType dishType, int rating, int difficulty, int cost, bool vegetarian, bool withAlcohol, PlanningEntry planEntry)
         {
@@ -83,56 +72,14 @@ namespace Check_n_Cook.Model
             this.PlanningEntry = planEntry;
         }
 
-        public async Task<bool> FileExistsAsync(StorageFolder folder, string fileName)
-        {
-            try
-            {
-                await folder.GetFileAsync(fileName);
-                return true;
-            }
-            catch (FileNotFoundException)
-            {
-                return false;
-            }
-        }
-        public async void AddToPlan(PlanningEntry planEntry) {
+        public void AddToPlan(PlanningEntry planEntry) {
 
 
 
-            MemoryStream stream1 = new MemoryStream();
 
-            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Receipe));
 
-            ser.WriteObject(stream1, this);
-            StringBuilder sb = new StringBuilder();
 
-                using (StreamReader sr = new StreamReader(stream1))
-                {
-                    sb.AppendLine(stream1.ToString());
 
-                }
-                StorageFolder localFolder = ApplicationData.Current.LocalFolder;
-                //StorageFile file = await StorageFile.GetFileFromApplicationUriAsync("ms-appdata:///local/file.txt"); 
-                // StreamWriteTo.WriteTo(stream1, "test.json");
-                StorageFile file;
-                int i = 1;
-
-                string filename = "receipe" + i + ".json";
-                    file = null;
-                    bool isFinished = false;
-                    while (await FileExistsAsync(ApplicationData.Current.RoamingFolder, filename) != false && isFinished != true)
-                    {
-                        i++;
-                        filename = "receipe" + i + ".json";
-                        if (await FileExistsAsync(ApplicationData.Current.RoamingFolder, filename) == false)
-                        {
-                            isFinished = true;
-                            await FileIO.WriteTextAsync(await ApplicationData.Current.RoamingFolder.GetFileAsync(filename), sb.ToString());
-
-                        }
-                    }
-                
-                
         }
         public void RemovePlanEntry()
         {
