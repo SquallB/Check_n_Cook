232661bc13fcfe765511d0dd403c8585fec3add2
SquallB <mazento2002@yahoo.fr>
2014-06-24 20:37:15 +0200

    view

--- Check_n_Cook/Views/View.cs
+++ Check_n_Cook/Views/View.cs
@@ -5,10 +5,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Interface that contains methods shared by all the views.
+/// </summary>
 namespace Check_n_Cook.Views
 {
     public interface View
     {
+        /// <summary>
+        /// Refreshes the view using the fired event.
+        /// </summary>
+        /// <param name="e">The fired event.</param>
         void Refresh(Event e);
     }
 }
