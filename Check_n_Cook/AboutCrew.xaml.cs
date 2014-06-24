7864196eb26253139cfa5ba7c08856179e55ac61
ScaY <stephane.viper@gmail.com>
2014-06-24 18:58:04 +0200

    Maj about crew

--- Check_n_Cook/AboutCrew.xaml.cs
+++ Check_n_Cook/AboutCrew.xaml.cs
@@ -2,69 +2,75 @@
 using Check_n_Cook.Model.Data;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Windows.Input;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 
 namespace Check_n_Cook
 {
     /// <summary>
-    /// Page affichant un titre de groupe, une liste des éléments contenus dans ce groupe, ainsi que les détails concernant
-    /// l'élément actuellement sélectionné.
+    /// Page displays a title for the group, a element list contains in this group, and the detail for the element selected
     /// </summary>
     public sealed partial class AboutCrew : Page
     {
+        /// <summary>
+        /// The navigation helper
+        /// </summary>
         private NavigationHelper navigationHelper;
+        /// <summary>
+        /// The default view model
+        /// </summary>
         private ObservableDictionary defaultViewModel = new ObservableDictionary();
 
         /// <summary>
         /// Cela peut être remplacé par un modèle d'affichage fortement typé.
         /// </summary>
+        /// <value>
+        /// The default view model.
+        /// </value>
         public ObservableDictionary DefaultViewModel
         {
             get { return this.defaultViewModel; }
         }
 
         /// <summary>
-        /// NavigationHelper est utilisé sur chaque page pour faciliter la navigation et 
+        /// NavigationHelper est utilisé sur chaque page pour faciliter la navigation et
         /// gestion de la durée de vie des processus
         /// </summary>
+        /// <value>
+        /// The navigation helper.
+        /// </value>
         public NavigationHelper NavigationHelper
         {
             get { return this.navigationHelper; }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AboutCrew"/> class.
+        /// </summary>
         public AboutCrew()
         {
             this.InitializeComponent();
 
-            // Configurer l'assistant de navigation
+            // Configure the navigation helper
             this.navigationHelper = new NavigationHelper(this);
             this.navigationHelper.LoadState += navigationHelper_LoadState;
 
-            // Configurer les composants de navigation de page logique qui permettent
-            // la page pour afficher un seul volet à la fois.
+            // Configure the navigation composant of the logic page that allow the
+            // page to display just one sub-page
             this.navigationHelper.GoBackCommand = new Check_n_Cook.Common.RelayCommand(() => this.GoBack(), () => this.CanGoBack());
             this.itemListView.SelectionChanged += itemListView_SelectionChanged;
 
-            // Commencer à écouter les modifications de la taille de la fenêtre 
-            // pour passer de l'affichage de deux volets à l'affichage d'un volet
             Window.Current.SizeChanged += Window_SizeChanged;
             this.InvalidateVisualState();
         }
 
+        /// <summary>
+        /// Handles the SelectionChanged event of the itemListView control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="SelectionChangedEventArgs"/> instance containing the event data.</param>
         void itemListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (this.UsingLogicalPageNavigation())
@@ -74,16 +80,16 @@ namespace Check_n_Cook
         }
 
         /// <summary>
-        /// Remplit la page à l'aide du contenu passé lors de la navigation.  Tout état enregistré est également
-        /// fourni lorsqu'une page est recréée à partir d'une session antérieure.
+        /// Fills the page with the previous elements while the navigation. Any state loaded is given when the page
+        /// is recreated from a previous session.
         /// </summary>
         /// <param name="sender">
-        /// La source de l'événement ; en général <see cref="NavigationHelper"/>
+        /// The event source ; generaly <see cref="NavigationHelper"/>
         /// </param>
-        /// <param name="e">Données d'événement qui fournissent le paramètre de navigation transmis à
-        /// <see cref="Frame.Navigate(Type, Object)"/> lors de la requête initiale de cette page et
-        /// un dictionnaire d'état conservé par cette page durant une session
-        /// antérieure.  L'état n'aura pas la valeur Null lors de la première visite de la page.</param>
+        /// <param name="e"> Event data that give the parameter of the navigation transmitted
+        /// <see cref="Frame.Navigate(Type, Object)"/> during the initial request of this page and
+        /// a state dictionnary preserved by this page during a previous session
+        /// The state will not take the value Null when the first visit of this page.</param>
         private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
         {
             List<ItemAbout> favorites = new List<ItemAbout>();
@@ -106,49 +112,22 @@ namespace Check_n_Cook
 
             if (e.PageState == null)
             {
-                // Quand il s'agit d'une nouvelle page, sélectionne automatiquement le premier élément, sauf si la navigation
-                // de page logique est utilisée (voir #region navigation de page logique ci-dessous.)
                 if (!this.UsingLogicalPageNavigation() && this.itemsViewSource.View != null)
                 {
                     this.itemsViewSource.View.MoveCurrentToFirst();
                 }
             }
-            else
-            {
-                // Restaure l'état précédemment enregistré associé à cette page
-                if (e.PageState.ContainsKey("SelectedItem") && this.itemsViewSource.View != null)
-                {
-                    // TODO: appelez Me.itemsViewSource.View.MoveCurrentTo() avec l'élément
-                    //       sélectionné, comme spécifié par la valeur de pageState("SelectedItem")
-
-                }
-            }
         }
-
-        /// <summary>
-        /// Conserve l'état associé à cette page en cas de suspension de l'application ou de la
-        /// suppression de la page du cache de navigation.  Les valeurs doivent être conformes aux
-        /// exigences en matière de sérialisation de <see cref="SuspensionManager.SessionState"/>.
-        /// </summary>
-        /// <param name="sender">La source de l'événement ; en général <see cref="NavigationHelper"/></param>
-        /// <param name="e">Données d'événement qui fournissent un dictionnaire vide à remplir à l'aide de
-        /// état sérialisable.</param>
 
         #region Navigation entre pages logiques
 
-        // La page fractionnée est conçue de sorte que si la fenêtre ne dispose pas d'assez d'espace pour afficher
-        // la liste et les détails, un seul volet s'affichera à la fois.
-        //
-        // Le tout est implémenté à l'aide d'une seule page physique pouvant représenter deux pages logiques.
-        // Le code ci-dessous parvient à ce but sans que l'utilisateur ne se rende compte de
-        // la distinction.
 
         private const int MinimumWidthForSupportingTwoPanes = 768;
 
         /// <summary>
-        /// Invoqué pour déterminer si la page doit agir en tant qu'une ou deux pages logiques.
+        /// Invoked to determinate if the page must display as one logic page or two.
         /// </summary>
-        /// <returns>True si la fenêtre doit agir en tant qu'une page logique, false
+        /// <returns>True if the page must display as one logic page, false
         /// .</returns>
         private bool UsingLogicalPageNavigation()
         {
@@ -156,30 +135,30 @@ namespace Check_n_Cook
         }
 
         /// <summary>
-        /// Appelé avec la modification de la taille de la fenêtre
+        /// Called with the modification of the size of the frame
         /// </summary>
-        /// <param name="sender">La fenêtre active</param>
-        /// <param name="e">Données d'événement décrivant la nouvelle taille de la fenêtre</param>
+        /// <param name="sender">The active frame</param>
+        /// <param name="e">Event data that give information about the new size of the frame</param>
         private void Window_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
         {
             this.InvalidateVisualState();
         }
 
         /// <summary>
-        /// Invoqué lorsqu'un élément d'une liste est sélectionné.
+        /// Called when a element of the list is selected.
         /// </summary>
-        /// <param name="sender">GridView qui affiche l'élément sélectionné.</param>
-        /// <param name="e">Données d'événement décrivant la façon dont la sélection a été modifiée.</param>
+        /// <param name="sender">GridView that displays the selected elementé.</param>
+        /// <param name="e">Event data that give information about how the selection have been modified.</param>
         private void ItemListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // Invalidez l'état d'affichage lorsque la navigation entre pages logiques est en cours, car une modification
-            // apportée à la sélection pourrait entraîner la modification de la page logique active correspondante.  Lorsqu'un
-            // élément est sélectionné, l'affichage passe de la liste d'éléments
-            // aux détails concernant l'élément sélectionné.  Lorsque cet élément est désélectionné, l'effet inverse
-            // est produit.
+
             if (this.UsingLogicalPageNavigation()) this.InvalidateVisualState();
         }
 
+        /// <summary>
+        /// Determines whether this instance [can go back].
+        /// </summary>
+        /// <returns></returns>
         private bool CanGoBack()
         {
             if (this.UsingLogicalPageNavigation() && this.itemListView.SelectedItem != null)
@@ -191,14 +170,13 @@ namespace Check_n_Cook
                 return this.navigationHelper.CanGoBack();
             }
         }
+        /// <summary>
+        /// Goes the back.
+        /// </summary>
         private void GoBack()
         {
             if (this.UsingLogicalPageNavigation() && this.itemListView.SelectedItem != null)
             {
-                // Lorsque la navigation entre pages logiques est en cours et qu'un élément est sélectionné,
-                // les détails de l'élément sont actuellement affichés.  La suppression de la sélection entraîne le retour à
-                // la liste d'éléments.  Du point de vue de l'utilisateur, ceci est un état visuel précédent logique
-                // de logique inversée.
                 this.itemListView.SelectedItem = null;
             }
             else
@@ -207,6 +185,9 @@ namespace Check_n_Cook
             }
         }
 
+        /// <summary>
+        /// Invalidates the state of the visual.
+        /// </summary>
         private void InvalidateVisualState()
         {
             var visualState = DetermineVisualState();
@@ -215,18 +196,16 @@ namespace Check_n_Cook
         }
 
         /// <summary>
-        /// Invoqué pour déterminer le nom de l'état visuel correspondant à l'état d'affichage
-        /// d'une application.
+        /// Called to determinate if the visual state name corresponding to the state of the display
+        /// of an application
         /// </summary>
-        /// <returns>Nom de l'état visuel désiré.  Il s'agit du même nom que celui
-        /// de l'état d'affichage, sauf si un élément est sélectionné dans l'affichage Portrait ou Snapped où
-        /// cette page logique supplémentaire est représentée par l'ajout du suffixe _Detail.</returns>
+        /// <returns>Visual state name desired. It is the same name that
+        /// the state of the display, expcet if an element is selected</returns>
         private string DetermineVisualState()
         {
             if (!UsingLogicalPageNavigation())
                 return "PrimaryView";
 
-            // Modifiez l'état d'activation du bouton Précédent lorsque l'état d'affichage est modifié
             var logicalPageBack = this.UsingLogicalPageNavigation() && this.itemListView.SelectedItem != null;
 
             return logicalPageBack ? "SinglePane_Detail" : "SinglePane";
@@ -236,20 +215,19 @@ namespace Check_n_Cook
 
         #region Inscription de NavigationHelper
 
-        /// Les méthodes fournies dans cette section sont utilisées simplement pour permettre
-        /// NavigationHelper pour répondre aux méthodes de navigation de la page.
-        /// 
-        /// La logique spécifique à la page doit être placée dans les gestionnaires d'événements pour  
-        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
-        /// et <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
-        /// Le paramètre de navigation est disponible dans la méthode LoadState 
-        /// en plus de l'état de page conservé durant une session antérieure.
-
+        /// <summary>
+        /// Called when the page is loaded and becomes the actual source of a parent frame.
+        /// </summary>
+        /// <param name="e">Event data that may be examinate by replcaing the code. The event data represents the navigation waiting that will load the active page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             navigationHelper.OnNavigatedTo(e);
         }
 
+        /// <summary>
+        /// Called immediately after the unload of the page and the page do not represent the actual source of the parent frame.
+        /// </summary>
+        /// <param name="e">Event data that may be examinate by replcaing the code. The event data represents the navigation waiting that will unload the active page.</param>
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
             navigationHelper.OnNavigatedFrom(e);
